74115c235cef5ef7050d6f932ee0e7285a1ec006
oliverfeher <oliver92.feher@gmail.com>
2020-03-12 00:11:34 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,7 @@
 class Post < ActiveRecord::Base
-
+    def create
+        Post.create(title: params[:post][:title], description: params[:post][:description])
+        redirect_to posts_path
+      end
+      
 end
